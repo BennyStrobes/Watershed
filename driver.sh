881dbba9b238fbb933d443a1a86036410d0bd7cf
BennyStrobes <bstrobe1@jhu.edu>
2019-09-24 14:44:55 -0400

    update driver.sh

--- driver.sh
+++ driver.sh
@@ -1,20 +1,3 @@
-
-
-
-#######################
-# Run 'evaluate_watershed.R'
-#######################
-# Run using RIVER
-model="Watershed_approximate"  # Can take on "Watershed_exact", "Watershed_approximate", "RIVER"
-number_of_dimensions="3" # Can take on any real number greater than or equal to one
-input_file="example_data/watershed_example_data.txt" # Input file
-output_prefix="model_"$model"_number_of_dimensions_"$number_of_dimensions
-Rscript evaluate_watershed.R --input $input_file --number_dimensions $number_of_dimensions --output_prefix $output_prefix --model_name $model
-
-
-
-
-if false; then
 # Run using Watershed approximate inference
 model="Watershed_approximate"  # Can take on "Watershed_exact", "Watershed_approximate", "RIVER"
 number_of_dimensions="3" # Can take on any real number greater than or equal to one
@@ -64,4 +47,4 @@ number_of_dimensions="3" # Can take on any real number greater than or equal to 
 input_file="example_data/watershed_example_data.txt" # Input file
 output_prefix="model_"$model"_number_of_dimensions_"$number_of_dimensions
 Rscript predict_watershed.R --training_input $input_file --prediction_input $input_file --number_dimensions $number_of_dimensions --output_prefix $output_prefix --model_name $model
-fi
+
